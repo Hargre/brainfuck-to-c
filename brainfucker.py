b9811a4c2eedeafb12d24baa1617ccc0d756dc4b
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-11-01 13:26:56 -0200

    Use a function dict instead of multiple ifs

--- brainfucker.py
+++ brainfucker.py
@@ -1,19 +1,14 @@
+funcmap = {
+    '>': lambda out_file: out_file.write('ptr++;\n'),
+    '<': lambda out_file: out_file.write('ptr--;\n'),
+    '+': lambda out_file: out_file.write('array[ptr]++;\n'),
+    '-': lambda out_file: out_file.write('array[ptr]--;\n'),
+    '.': lambda out_file: out_file.write('putchar(array[ptr]);\n'),
+    ',': lambda out_file: out_file.write('getchar(array[ptr]);\n'),
+    '[': lambda out_file: out_file.write('while (array[ptr]) {\n\t'),
+    ']': lambda out_file: out_file.write('}'),
+}
+
 def parse_bf(symbol, out_file):
-    if symbol == '>':
-        out_file.write('ptr++;\n')
-    elif symbol == '<':
-        out_file.write('ptr--;\n')
-    elif symbol == '+':
-        out_file.write('array[ptr]++;\n')
-    elif symbol == '-':
-        out_file.write('array[ptr]--;\n')
-    elif symbol == '.':
-        out_file.write('putchar(array[ptr]);\n')
-    elif symbol == ',':
-        out_file.write('getchar(array[ptr]);\n')
-    elif symbol == '[':
-        out_file.write('while (array[ptr]) {\n\t')
-    elif symbol == ']':
-        out_file.write('}')
-    else:
-        raise ValueError("Invalid character!")
+    if symbol in funcmap:
+        funcmap[symbol](out_file)
